9c7bd811c99cab61aba3a27a2f46d358a3e8889a
Stallings <CS051376@cerner.net>
2019-12-18 20:55:51 -0600

    committing studio 3 for angular

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -21,11 +21,20 @@ export class EquipmentComponent implements OnInit {
    cargoMass: number = 0;
    maximumAllowedMass: number = 2000;
    maxItems: number = 10;
+   
 
    constructor() { }
 
    ngOnInit() { }
 
-   // Code your addItem function here:
+   addItem(equipment: object[]) {
+     this.cargoHold.push(equipment);
+     this.cargoMass += equipment['mass'];
+     if (this.maximumAllowedMass - this.cargoMass <= 200) {
+       return true
+     } else {
+       return false
+     }
+   }
    
 }
